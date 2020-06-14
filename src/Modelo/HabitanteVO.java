1cc886e497d895c8cae47c68e0a4a54fedc07c33
Angel Alexander Jimenez <Usuario@Angel-Alexander>
2020-06-14 11:49:36 -0400

    clase preparada y lista!

--- src/Modelo/HabitanteVO.java
+++ src/Modelo/HabitanteVO.java
@@ -7,8 +7,80 @@ package Modelo;
 
 /**
  *
- * @author Usuario
+ * @author Angel Alexander Jimenez
  */
 public class HabitanteVO {
     
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String fechaNacimiento;
+    private int edad;
+
+    public HabitanteVO() {
+    }
+
+    public HabitanteVO(String primerNombre,
+                       String segundoNombre,
+                       String primerApellido,
+                       String segundoApellido,
+                       String fechaNacimiento,
+                       int edad) {
+        
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }

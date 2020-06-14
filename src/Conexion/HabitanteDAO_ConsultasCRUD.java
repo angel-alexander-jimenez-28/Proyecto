1330a82433fcd9a4034a77626f62b9315684f598
Angel Alexander Jimenez <Usuario@Angel-Alexander>
2020-06-14 11:51:43 -0400

    clase preparada y lista!

--- src/Conexion/HabitanteDAO_ConsultasCRUD.java
+++ src/Conexion/HabitanteDAO_ConsultasCRUD.java
@@ -4,11 +4,62 @@
  * and open the template in the editor.
  */
 package Conexion;
+import static Conexion.Conexion.con;
+import static Conexion.Conexion.getConexion;
+import static Conexion.Conexion.pstm;
+import static Conexion.Conexion.rs;
+import Modelo.UsuarioVO;
+import java.sql.SQLException;
 
 /**
  *
- * @author Usuario
+ * @author Angel Alexander Jimenez
+ * 
  */
-public class HabitanteDAO_ConsultasCRUD {
+public class HabitanteDAO_ConsultasCRUD extends Conexion{
+    
+    
+    private static boolean Existe(UsuarioVO usuario) throws SQLException {
+        String sql = "SELECT * FROM habitante WHERE id_cedula_h=? ";
+        pstm = con.prepareStatement(sql);
+        pstm.setString(1, usuario.getCedula());
+        rs = ExecuteSearch(pstm);
+        if(rs.next()) return true;
+        else return false;
+    }
+    
+    public static boolean Registrar(UsuarioVO usuario) {
+        con = getConexion();
+        String sql="INSERT INTO usuario"
+                 + "(id_Usuario_Cedula,"
+                 +   "Primer_Nombre,Segundo_Nombre,"
+                 +   "Primer_Apellido,Segundo_Apellido,"
+                 +   "Correo,Nombre_Acceso,Clave_Acceso)"
+                 + "VALUES(?,?,?,?,?,?,?,?)";
+        try {
+            if(!Existe(usuario)){
+                pstm = con.prepareStatement(sql);
+                pstm.setString(1, usuario.getCedula());
+                pstm.setString(2, usuario.getNombre1());
+                pstm.setString(3, usuario.getNombre2());
+                pstm.setString(4, usuario.getApellido1());
+                pstm.setString(5, usuario.getApellido2());
+                pstm.setString(6, usuario.getCorreo());
+                pstm.setString(7, usuario.getNombreUsuario());
+                pstm.setString(8, usuario.getClaveUsuario());
+                ExecuteInsert(pstm);
+                return true;
+            }else return false;
+        } catch (SQLException e) {
+            System.err.println("en el insert "+e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
     
 }

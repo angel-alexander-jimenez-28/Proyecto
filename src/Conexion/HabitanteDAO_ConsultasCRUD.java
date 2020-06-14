84c702ad1c27f074c6b024c89b3aa4948c06a3f9
Angel Alexander Jimenez <Usuario@Angel-Alexander>
2020-06-14 13:40:04 -0400

    solo quite uno comentarios que genera netbeans

--- src/Conexion/HabitanteDAO_ConsultasCRUD.java
+++ src/Conexion/HabitanteDAO_ConsultasCRUD.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Conexion;
 import static Conexion.Conexion.con;
 import static Conexion.Conexion.getConexion;
